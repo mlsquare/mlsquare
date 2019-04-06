1030e1644e97cdf92af69e0acd438d146dcea57b
Ravi Mula <ravishankarmula@gmail.com>
2019-04-06 20:23:11 +0530

    Release Version

--- src/mlsquare/__init__.py
+++ src/mlsquare/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-__version__ = 'v0.1.0'
+from pkg_resources import get_distribution, DistributionNotFound
+
+__version__ = 'v0.1'
 __author__ = "MLSquare"
 __copyright__ = "MLSquare"
 __license__ = "mit"
