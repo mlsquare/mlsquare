420880937b9b3922b4041925fda70d7e0d38ba7b
Ravi Mula <ravishankarmula@gmail.com>
2019-04-06 20:26:31 +0530

    Release Version

--- src/mlsquare/__init__.py
+++ src/mlsquare/__init__.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import get_distribution, DistributionNotFound
 
-__version__ = 'v0.1'
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+# __version__ = 'v0.1.1'
 __author__ = "MLSquare"
 __copyright__ = "MLSquare"
 __license__ = "mit"
