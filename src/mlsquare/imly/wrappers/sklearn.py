1fc2a68584086c1de6e947b7b69eac5bbc0640a9
shakkeel1330 <shakkeelahammed@gmail.com>
2019-04-20 15:07:39 +0530

    Added DNDT mapping

--- src/mlsquare/imly/wrappers/sklearn.py
+++ src/mlsquare/imly/wrappers/sklearn.py
@@ -120,6 +120,95 @@ class SklearnKerasRegressor(KerasRegressor):
             onnxmltools.utils.save_model(onnx_model, filename + '.onnx')
 
 
+class SklearnTensorflowClassifier():
+        def __init__(self, build_fn, **kwargs):
+            self.build_fn = build_fn
+            self.primal = kwargs['primal']
+            self.params = kwargs['params']
+            self.best = kwargs['best']
+
+        def score():
+            pass
+
+        def predict():
+            pass
+
+        def predict_proba():
+            pass
+
+        def fit(self, x_train, y_train, **kwargs):
+            import numpy as np
+            import tensorflow as tf
+            kwargs.setdefault('batch_size', 500)
+            kwargs.setdefault('epochs', 500)
+            kwargs.setdefault('verbose', 0)
+            verbose = kwargs['verbose']
+            kwargs.setdefault('params', self.params)
+            kwargs.setdefault('space', False)
+
+            y_train = np.array(y_train) # Compatibility with all formats?
+            if len(y_train.shape) == 2 and y_train.shape[1] > 1:
+                self.classes_ = np.arange(y_train.shape[1])
+            elif (len(y_train.shape) == 2 and y_train.shape[1] == 1) or len(y_train.shape) == 1:
+                self.classes_ = np.unique(y_train)
+                y_train = np.searchsorted(self.classes_, y_train)
+            else:
+                raise ValueError(
+                        'Invalid shape for y_train: ' + str(y_train.shape))
+            # Check whether to compute for the best model or not
+            # if (self.best and kwargs['params'] != self.params and kwargs['space']):
+            #     # Optimize
+            #     hyperopt_space = kwargs['space']
+            #     self.params.update(kwargs['params'])
+
+            #     primal_model = self.primal
+            #     primal_model.fit(x_train, y_train)
+            #     y_pred = primal_model.predict(x_train)
+            #     primal_data = {
+            #         'y_pred': y_pred,
+            #         'model_name': primal_model.__class__.__name__
+            #     }
+
+            #     ## Search for best model using Tune ##
+            #     self.model = get_best_model(x_train, y_train,
+            #         primal_data=primal_data, params=self.params, space=hyperopt_space)
+            #     self.model.fit(x_train, y_train, epochs=200,
+            #                 batch_size=30, verbose=verbose) # Not necessary. Fitting twice by now.
+            #     return self.model # Not necessary.
+            # else:
+                # Dont Optmize
+            
+            self.init_ops = self.build_fn.__call__(x_train=x_train)
+
+            with tf.Session() as sess:
+                # initialise the variables
+                sess.run(self.init_ops)
+                total_batch = int(len(y_train) / kwargs['batch_size'])
+                for epoch in range(kwargs['epochs']):
+                        avg_cost = 0
+                        for i in range(total_batch):
+                            batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
+                            _, c = sess.run([optimiser, cross_entropy], 
+                                        feed_dict={x: batch_x, y: batch_y})
+                            avg_cost += c / total_batch
+                        print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
+                print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+
+            # self.model.fit(x_train, y_train, epochs=500, batch_size=500, verbose=verbose)
+            # return self.model # Not necessary.
+
+        def save(self, filename = None):
+            if filename == None:
+                raise ValueError('Name Error: to save the model you need to specify the filename')
+
+            pickle.dump(self.model, open(filename + '.pkl', 'wb'))
+
+            self.model.save(filename + '.h5')
+            
+            onnx_model = onnxmltools.convert_keras(self.model)
+            onnxmltools.utils.save_model(onnx_model, filename + '.onnx')
+
+
 wrappers = {
     'LogisticRegression': SklearnKerasClassifier,
     'LinearRegression': SklearnKerasRegressor,
