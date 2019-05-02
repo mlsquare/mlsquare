bb3176e3c1461544c2cb4a836bb71b59d7ee9ee4
Shakkeel <shakkeelahammed@gmail.com>
2019-05-02 12:23:39 +0530

    Added Decision tree layer

--- src/mlsquare/imly/commons/custom_layers.py
+++ src/mlsquare/imly/commons/custom_layers.py
@@ -2,14 +2,16 @@ from keras.layers import Layer
 
 class Bin(Layer):
     """
-    A custom keras layer to create bins in DNDT implementation(https://arxiv.org/pdf/1806.06988.pdf).
+    A custom keras layer to create bins in
+    DNDT implementation(https://arxiv.org/pdf/1806.06988.pdf).
     The input for this layer should always be the Keras 'Input' layer.
-    The layer then accepts a feature of your choice(index_of_feature) and the number of bins(num_of_cuts + 1)
-    you want the selected feature to be split into.
-    For a given input of shape (None, features) and cut points = D, this layer returns a tensor
-    of shape (None, D+1).
+    The layer then accepts a feature of your choice(index_of_feature) and
+    the number of bins(num_of_cuts + 1) you want the selected feature
+    to be split into.
+    For a given input of shape (None, features) and cut points = D, this layer
+    returns a tensor of shape (None, D+1).
 
-    # Properties
+    # Arguments
         index_of_feature: Integer, The position of your choice of feature to be split into bins.
         num_of_cuts: Integer, The number of bins you want the feature to be split into.
 
@@ -50,8 +52,8 @@ class KronProd(Layer):
 
     """
     A layer to compute the Kron product.
-    This layer expects a list of tensors as input. Kronecker product operation is then applied 
-    in sequence to each element in the list.
+    This layer expects a list of tensors as input. Kronecker product
+    operation is then applied in sequence to each element in the list.
 
     """
 
@@ -82,32 +84,64 @@ class KronProd(Layer):
 
 from functools import reduce
 
-class DecisionTrees2(Layer):
+class DecisionTree(Layer):
 
-    def __init__(self, num_cut, **kwargs):
-        self.num_cut = num_cut
-        self.cut_points_list = []
-        super(DecisionTrees2, self).__init__(**kwargs)
+    """
+    A custom Keras layer that implements the
+    Deep Neural Decision Tree(https://arxiv.org/pdf/1806.06988.pdf).
+    On initialization the layer expects the number of cuts you
+    would like to make per feature.
+
+    Note: The input expected is of rank 2.
+
+    # Arguments
+        cuts_per_feature: List of integers, each integer is the number
+        of cuts to be made on it's corresponding feature.
+
+
+    # Example
+
+        # Creating a model with DecisionTree layer using
+        # Keras functional API
+        visible = Input(shape=(4,))
+        hidden_layer = DecisionTree(cuts_per_feature=[1,1,1,1])(visible)
+        output = Dense(3, activation='sigmoid')(hidden_layer)
+        model = Model(inputs=visible, ouputs=output)
+
+        # The above model accepts an input with (None, 4) shape. The Decsion tree layer
+        # then provides an output of shape (None, 16) - 2 bins per feature in this scenario.
+        # The model then applies a regular 'Dense' layer to this output.
+
+    # Input
+        2D tensor with shape: `(batch_size, num_of_feature)`.
+
+    # Output
+        2D tensor with shape: `(batch_size, num_of_bins)`.
+        num_of_bins = prod(cuts_per_feature + 1)
+    """
+
+    def __init__(self, cuts_per_feature, **kwargs):
+        self.cuts_per_feature = cuts_per_feature
+        super(DecisionTree, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        for i in self.num_cut:
-          self.cut_points_list.append(self.add_weight(name='cut_points'+'_'+str(i), # Validate this choice of trainable weight.
-                                        shape=(i,), # Or (self.num_cut,)
+        self._trainable_cutpoints = []
+        for i in self.cuts_per_feature:
+          self._trainable_cutpoints.append(self.add_weight(name='cut_points'+'_'+str(i),
+                                        shape=(i,),
                                         initializer='uniform',
                                         trainable=True))
-        super(DecisionTrees2, self).build(input_shape)  # Be sure to call this at the end
+        super(DecisionTree, self).build(input_shape)
         
         
-    def bin_fn(self,feature_num, num_cut_value, x):
+    def binning_fn(self,index_of_feature, num_of_cuts, x, temperature = 0.1):
         from keras import backend as K # Fix
         import tensorflow as tf
-        temperature = 0.1
-        X = x[:, feature_num - 1 : feature_num]
-        # D = self.cut_points.get_shape().as_list()[0]
-        D = num_cut_value
-        W = K.reshape(tf.linspace(1.0, D + 1.0, D + 1), [1, -1]) # find 'K' equivalent of linspace
-        cut_points_value = tf.contrib.framework.sort(self.cut_points_list[feature_num - 1])  # change tf
-        b = K.cumsum(tf.concat([K.constant(0.0, shape=[1]), -cut_points_value], 0))
+        X = x[:, index_of_feature : index_of_feature + 1]
+        D = num_of_cuts
+        W = K.reshape(tf.linspace(1.0, D + 1.0, D + 1), [1, -1])
+        cutpoints_value = tf.contrib.framework.sort(self._trainable_cutpoints[index_of_feature - 1])
+        b = K.cumsum(tf.concat([K.constant(0.0, shape=[1]), -cutpoints_value], 0))
         h = tf.matmul(X, W) + b
         output = tf.nn.softmax(h / temperature)
         return output
@@ -120,18 +154,21 @@ class DecisionTrees2(Layer):
           res = tf.reshape(res, [-1, tf.reduce_prod(res.shape[1:])])
           return res
       
-      binning_layer_list = []
-      for i, value in enumerate(self.num_cut):
-        binning_layer_list.append(
-            self.bin_fn(feature_num=i+1, num_cut_value=value, x=x)
+      bin_result = []
+      for i, value in enumerate(self.cuts_per_feature):
+        bin_result.append(
+            self.binning_fn(index_of_feature=i, num_of_cuts=value, x=x)
         )
 
-#       output = KronProd(cut_points_list=self.num_cut)(binning_layer_list) # Remove layer, replace as logic
-      print(len(binning_layer_list))
-      output = reduce(kron_prod, binning_layer_list)
+      output = reduce(kron_prod, bin_result)
       self.output_dim = output.get_shape().as_list()
 
       return output
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.output_dim[1])
+
+## TODO
+# Default cutpoints - sealing function
+# Error handling in layers
+# Fix multiple imports
